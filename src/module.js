742daff0a69ab4d5a20e83f3a1d6c7b1482f2cbe
Adam Gedney <adam@olympussat.com>
2017-09-22 13:04:57 -0400

    UPDATED state tree merge strategy

--- src/module.js
+++ src/module.js
@@ -2,16 +2,15 @@ import {
   INJECT_DATA
 } from './mutation-types.js';
 
-const mapKeyToStateBranch = (d,m)=>{
-  for(let k in m){
-    if(d && d.hasOwnProperty(k)){
-      return {
-        branch: m[k],
-        key : k
-      };
-    }
+const testMap = [
+  {
+    branch : 'bid',
+    key : 'getBidById',
+    updateByMerging: true //defaults to replace. Note: Arrays are concatenated
   }
-};
+];
+
+const mapKeyToStateBranch = (d,m)=>m.filter(item => d && d.hasOwnProperty(item.key));
 
 // actions
 export const actions = {
@@ -25,15 +24,19 @@ export const mutations = {
   [INJECT_DATA] (state, { data, map, options }) {
     const mapped = mapKeyToStateBranch(data,map);
 
-    if(state.hasOwnProperty(mapped.branch)){
+    /**
+     * Merge or replace based on assigned update strategy as assigned in the map
+     * Handles arrays or objects
+     */
+    if(state.hasOwnProperty(mapped.branch) && mapped.updateByMerging){
       if(Array.isArray(state[mapped.branch])){
-        // Diff the array here, concatenate, or replace?
+          state[mapped.branch] = state[mapped.branch].concat(data[mapped.key]);
+      }else{
+          state[mapped.branch] = {...state[mapped.branch], ...data[mapped.key]};
       }
     }else{
-      state[branch] = data[mapped.key];
+      state[mapped.branch] = data[mapped.key];
     }
-    // 
-    // state.count = state.count + amount
   }
 }
 
